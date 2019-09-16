b14cc757c054b93a19b5018b04f03aff119332d3
dianlujitao <dianlujitao@lineageos.org>
2019-09-16 17:40:51 +0800

    onyx: Create system copy of ims-ext-common and add to classpath
    Change-Id: Ia530a530bd4469e12500410fc5e8ba393de60b18

--- device.mk
+++ device.mk
@@ -281,6 +281,13 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common_system
+
+PRODUCT_BOOT_JARS += \
+    ims-ext-common_system
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
