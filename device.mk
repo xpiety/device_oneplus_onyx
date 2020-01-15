3d99c1cd6b90b9137a5fc1706b836c11d662958b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-01-15 09:24:26 +0530

    onyx: Add privapp permission for Snap
    from https://github.com/LineageOS/android_vendor_lineage/blob/lineage-17.1/config/permissions/privapp-permissions-lineage-system.xml
    
    Change-Id: If108987122bc0928dab3c897febf84702338b6df

--- device.mk
+++ device.mk
@@ -65,6 +65,7 @@ PRODUCT_COPY_FILES += \
 
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-lineage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lineage.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # ANT+
